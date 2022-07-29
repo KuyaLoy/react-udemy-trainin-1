f0ee7ed0a5fb771dd1ec7536e7d135be012422e1
robin tapiru <robin@tncitgroup.com>
2022-07-29 13:50:58 +0400

    a

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ class App extends Component {
 
     return(
       <div className='App'>
+        <h1 className="app-title">Monster Rolodex</h1>
         <SearchBox onChangeHandler={onSearchChange} placeholder={'Search Monster'} className={'monster-search-box'}/>
         <CardList monsters={filteredMonsters}/> 
       </div>
